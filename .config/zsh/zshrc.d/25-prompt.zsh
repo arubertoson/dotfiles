5af1e0ddfaa778efbd801755a2996368f492f9d3
Marcus Albertsson <marcus.arubertoson@gmail.com>
2017-03-12 08:18:56 +0000

    compton, prompt fixes and added packages to arch

--- .config/zsh/zshrc.d/25-prompt.zsh
+++ .config/zsh/zshrc.d/25-prompt.zsh
@@ -2,19 +2,16 @@
 # Prompt Pure
 #
 
-# Add timestamp in front of prompt
-PROMPT='%F{white}%* '$PROMPT
-
-# Custom cursor colors
+# Change color on cursor depending on vi mode
 ZSH_VICMD_CURSOR="#51afef"
 ZSH_VIINS_CURSOR="#DFDFDF"
 
-# Change color cursor with vi-mode
 zsh-keymap-select-vicursor() {
   if [[ $KEYMAP == vicmd ]]; then
     echo -ne "\e[1 q"
     echo -ne "\033]12;$ZSH_VICMD_CURSOR\033\\"
   else
+    # If we want a beam in insert mode uncomment
     # echo -ne "\e[5 q"
     echo -ne "\033]12;#DFDFDF\033\\"
   fi
@@ -26,3 +23,17 @@ function zle-line-init zle-keymap-select {
 }
 zle -N zle-line-init
 zle -N zle-keymap-select 
+
+#
+# Modify Prompt
+#
+
+function bg_jobs() {
+  echo "%{$fg[yellow]%}%(1j.[+%j] .)"
+}
+
+function time_() {
+  echo "%F{white}%* "
+}
+
+PROMPT='$(bg_jobs)$(time_)'$PROMPT
