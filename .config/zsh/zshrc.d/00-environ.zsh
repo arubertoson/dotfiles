367e6c8e566482d72a36c7d08cd146b70f85ddf9
Marcus Albertsson <marcus.arubertoson@gmail.com>
2017-03-20 00:27:14 +0100

    fix polybar, dunst, alacritty. Small bugs in spacemacs and zsh settings

--- .config/zsh/zshrc.d/00-environ.zsh
+++ .config/zsh/zshrc.d/00-environ.zsh
@@ -2,37 +2,28 @@
 # Environment variables for session and apps
 # 
 
-function filter-engine() {
-  echo -n "Setting main filter engine..."
-  export FILTER_ENGINE
-  declare -a arr=("fzy" "fzf" "peco" "percol") 
-  for filter in "${arr[@]}"
-  do
-    if (( $+commands[$filter] ))
-    then
-      echo " $filter"
-      FILTER_ENGINE=$filter
-      break
-    fi
-  done
-}
 
-function tig() {
-  if (( $+commands[tig] ))
+export FILTER_ENGINE
+declare -a arr=("fzy" "fzf" "peco" "percol") 
+for filter in "${arr[@]}"
+do
+  if (( $+commands[$filter] ))
   then
-    export TIGRC_USER="$XDG_CONFIG_HOME/tig/config"
+    FILTER_ENGINE=$filter
+    break
   fi
-}
+done
 
-function npm() {
-  if (( $+commands[npm] ))
-  then
-    export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
-    export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
-    export NPM_CONFIG_PREFIX="$XDG_DATA_HOME/npm"
-  fi
-}
 
-filter-engine
-tig
-npm
+if (( $+commands[tig] ))
+then
+export TIGRC_USER="$XDG_CONFIG_HOME/tig/config"
+fi
+
+if (( $+commands[npm] ))
+then
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/config"
+export NPM_CONFIG_CACHE="$XDG_CACHE_HOME/npm"
+export NPM_CONFIG_PREFIX="$XDG_DATA_HOME/npm"
+fi
+
--- .config/zsh/zshrc.d/25-prompt.zsh
+++ .config/zsh/zshrc.d/25-prompt.zsh
@@ -17,9 +17,22 @@ zsh-keymap-select-vicursor() {
   fi
 }
 
+zsh-update-vim-prompt() {
+    zle || {
+        print "error: pure_update_vim_prompt must be called when zle is active"
+        return 1
+    }
+
+    #PURE_PROMPT_SYMBOL=${${KEYMAP/vicmd/«}/(main|viins)/»}
+    PURE_PROMPT_SYMBOL=${${KEYMAP/vicmd/❮}/(main|viins)/❯}
+    zle .reset-prompt
+}
+
+
 # Hook to zle functions
 function zle-line-init zle-keymap-select { 
-    zsh-keymap-select-vicursor
+    #zsh-keymap-select-vicursor
+    zsh-update-vim-prompt
 }
 zle -N zle-line-init
 zle -N zle-keymap-select 
@@ -36,4 +49,9 @@ function time_() {
   echo "%F{white}%* "
 }
 
-PROMPT='$(bg_jobs)$(time_)'$PROMPT
+# ❯ »
+PURE_PROMPT_SYMBOL='>'
+PURE_GIT_DOWN_ARROW='↑'
+PURE_GIT_UP_ARROW='↓'
+PROMPT='$(bg_jobs)$(time_)%(?.%F{magenta}.%F{red})${PURE_PROMPT_SYMBOL}%f '
+#PROMPT='$(bg_jobs)$(time_)'$PROMPT
