b5093d827899a3a018a4ceb324fe2274acc5c761
Marcus Albertsson <2284432+arubertoson@users.noreply.github.com>
2026-06-05 19:55:00 +0200

    fix(prompt): redraw prompt on vi mode changes
    Ensure the prompt updates immediately when switching between vi insert and command modes, even when the cursor mode widget overrides zle-keymap-select.

--- rc.d/conf.plug/zsh-vi-mode.zsh
+++ rc.d/conf.plug/zsh-vi-mode.zsh
@@ -9,10 +9,15 @@ bindkey -M vicmd 'N' history-search-backward
 echo -en '\x1b[6 q'
 
 cursor-set-vi-mode() {
-    case $KEYMAP in
-        vicmd) echo -en '\x1b[2 q' ;;
-        main|viins) echo -en '\x1b[6 q' ;;
-    esac
+  case $KEYMAP in
+    vicmd) echo -en '\x1b[2 q' ;;
+    main|viins) echo -en '\x1b[6 q' ;;
+  esac
+
+  if (( $+functions[prompt_fruz_update_prompt] )); then
+    prompt_fruz_update_prompt
+    zle .reset-prompt
+  fi
 }
 
 zle -N zle-keymap-select cursor-set-vi-mode
