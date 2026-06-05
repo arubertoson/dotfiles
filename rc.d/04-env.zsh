9dd4d2af3598d1ee6b90e6e2a5de2fd5f3c97cd3
Marcus Albertsson <2284432+arubertoson@users.noreply.github.com>
2026-06-05 12:22:56 +0200

    Update shell tooling defaults

--- rc.d/04-env.zsh
+++ rc.d/04-env.zsh
@@ -11,11 +11,15 @@ export -UT INFOPATH infopath  # -T creates a "tied" pair; see below.
 # ZK
 export ZK_NOTEBOOK_DIR=~/notes
 
+# Prevent Ctrl+D from exiting the shell accidentally (requires 2 presses)
+# Or set it to 0 if you want it to never exit via Ctrl+D
+export IGNOREEOF=0
+
 # $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
 # Modifying one will also modify the other.
 path=(
-    $path
     ~/.local/bin
+    $path
     $WIN32YANK
 )
 
